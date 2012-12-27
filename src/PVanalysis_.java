9928ecc58a0c67947dc7f655d4bfb064a8fd6958
KatjaSchulze <kschulze@th-wildau.de>
2012-12-27 14:36:44 +0100

    first commit

--- src/PVanalysis_.java
+++ src/PVanalysis_.java
@@ -1,6 +1,8 @@
 /**
 Copyright (C) 2012  Katja Schulze (kschulze@th-wildau.de)
 
+All other copyrights are the property of their respective owners.
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
